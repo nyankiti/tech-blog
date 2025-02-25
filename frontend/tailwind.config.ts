ad9440ed0911457613e33127c12b190d82618bbd
nyankiti <nyancic77@outlook.jp>
2025-02-26 04:01:02 +0900

    fix tailwind config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -5,7 +5,7 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "../blog-contents/contents/tech-bl0g/*.{ts,tsx,md,mdx}",
+    "./blog-contents/**/*.{js,jsx,ts,tsx,md,mdx}",
   ],
   theme: {
     extend: {
