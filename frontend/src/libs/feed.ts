b5feef2eb47fefacdd99102d7979872709c07781
nyankiti <nowi41cic77mav@gmail.com>
2025-03-09 11:59:23 +0900

    update netlify.toml

--- frontend/src/libs/feed.ts
+++ frontend/src/libs/feed.ts
@@ -1,14 +1,14 @@
-import { readFile } from "node:fs/promises";
+// import { readFile } from "node:fs/promises";
 import { Feed } from "feed";
-import { FrontMatter } from "./posts";
+// import { FrontMatter } from "./posts";
 import { SITE_DESCRIPTION, SITE_TITLE, SITE_URL } from "@/constants";
-import { POSTS_JSON_PATH } from "@/libs/generate-posts-json";
+// import { POSTS_JSON_PATH } from "@/libs/generate-posts-json";
 
 export const generateFeed = async () => {
-  const postsJsonString = await readFile(POSTS_JSON_PATH, "utf-8");
-  const posts = JSON.parse(postsJsonString) as (FrontMatter & {
-    content: string;
-  })[];
+  // const postsJsonString = await readFile(POSTS_JSON_PATH, "utf-8");
+  // const posts = JSON.parse(postsJsonString) as (FrontMatter & {
+  //   content: string;
+  // })[];
 
   const feed = new Feed({
     id: SITE_URL.toString(),
@@ -20,18 +20,18 @@ export const generateFeed = async () => {
     favicon: new URL("/favicon.ico", SITE_URL).toString(),
   });
 
-  posts.forEach((post) => {
-    feed.addItem({
-      title: post.title,
-      id: new URL(`/blog/${post.slug}`, SITE_URL).toString(),
-      link: new URL(`/blog/${post.slug}`, SITE_URL).toString(),
-      date: new Date(post.publishedAt),
-      image: {
-        url: new URL(`/blog/${post.slug}/opengraph-image`, SITE_URL).toString(),
-        type: "image/png",
-      },
-    });
-  });
+  // posts.forEach((post) => {
+  //   feed.addItem({
+  //     title: post.title,
+  //     id: new URL(`/blog/${post.slug}`, SITE_URL).toString(),
+  //     link: new URL(`/blog/${post.slug}`, SITE_URL).toString(),
+  //     date: new Date(post.publishedAt),
+  //     image: {
+  //       url: new URL(`/blog/${post.slug}/opengraph-image`, SITE_URL).toString(),
+  //       type: "image/png",
+  //     },
+  //   });
+  // });
 
   return feed.rss2();
 };
